c5eee33175b01bc3bbc322aa0a70f64c9d298607
MufaddalHamid <mufaddalhhamid@gmail.com>
2023-11-11 00:10:13 +0530

    First Commit adding static folders and files

---
+++ app.py
@@ -0,0 +1,23 @@
+import uuid
+import requests
+from flask import Flask, jsonify, render_template
+import os
+
+import os
+app = Flask(__name__, template_folder='Views', static_url_path='/static/')
+
+# region Index
+@app.route("/")
+def home():
+    if 'Login' == 'Student':
+    return render_template('Forms/index.html')
+# endregion
+
+# region Login
+@app.route("/Login")
+def Login():
+    return render_template('Forms/Login.html')
+# endregion
+
+if __name__ == "__main__":
+    app.run(debug=True)
