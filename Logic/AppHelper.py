8d450fc6a651afacd802f992676b463c57aa1e36
Sourav Kumar <31382363+studentdevelops@users.noreply.github.com>
2023-11-16 15:09:35 +0530

    made changes to Login Page, added method to create auth token and add them to the db, needs slight changes, made method to set and remove auth_tokens from client side, check TODOs for the changes to be made

--- Logic/AppHelper.py
+++ Logic/AppHelper.py
@@ -4,8 +4,30 @@ from sqlalchemy import create_engine, text, select
 from Datamodel.BaseDM import Base
 from DataModel import AuthControl
 
-from flask import request
-# endRegion
+import secrets
+from datetime import datetime, timedelta
+from flask import request, make_response
+
+def set_cookie():
+    resp = make_response()
+    auth_token = create_auth_token()
+    resp.set_cookie('auth_token', auth_token, expires= datetime.utcnow() + timedelta(days=2))
+    return resp
+
+def remove_cookie():
+    resp = make_response()
+    resp.set_cookie('auth_token', '', expires=datetime.utcnow() - timedelta(days= -2))
+    return resp
+
+def create_auth_token(session, length=64):
+    auth_token = secrets.token_hex(length)
+    # todo 1: Add the auth_token to db
+    # need session object again
+    # insert it into db
+    # todo 2: set role for auth token by fetching the respective role from db
+    auth_control = AuthControl(Auth_token=auth_token, role="SET ROLE", expiry=(datetime.utcnow() + timedelta(days=2))) 
+    session.add(auth_control)
+    return auth_token
 
 def delete_old_auth_Tokens(session):
     # PostgreSQL or MYSQL uses CURRENT_TIMESTAMP or NOW()
@@ -15,10 +37,8 @@ def delete_old_auth_Tokens(session):
     session.execute(text('DELETE FROM "Auth_Access_Control" WHERE expiry < CURRENT_TIMESTAMP'))
     session.commit()
 
-def checkRole(session):
-
+def checkRole(request, session):
     cookies = request.cookies
-
     if ( 'auth_token' in cookies ):
         auth_token_value = cookies['auth_token']
         stmt = select(AuthControl).where(AuthControl.Auth_token == auth_token_value)
@@ -28,6 +48,8 @@ def checkRole(session):
         else: 
             return False
         # session.query(AuthControl).all()
+    else:
+        return False
     
 
 class ActiveSession:
